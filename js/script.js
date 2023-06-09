8da20e9193ff9723c7a9a0b7d4ba625dcac25336
FraFra2 <kekkino41@gmail.com>
2023-06-09 14:35:39 +0200

    Creazione html e script js e esercizio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//HTML Elements
+const label = document.getElementById("passwordLabel");
+console.log(label);
+//User Input
+const userName = prompt("Inserire il proprio nome:");
+const userSecondName = prompt("Inserire il proprio cognome:");
+const favCol = prompt("Inserire il proprio colore preferito:");
+
+let pass = userName + userSecondName + favCol + "101";
+
+let output = `La tua (insicurissima) password: ${pass}`;
+
+label.innerHTML = output;
+
+
